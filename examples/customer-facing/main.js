7d9df206940e15b5f6be2c068e580af77ef12dcc
Ritchie <ritchieleeann@gmail.com>
2014-08-13 14:01:36 -0700

    added knobs

--- examples/customer-facing/main.js
+++ examples/customer-facing/main.js
@@ -3,6 +3,13 @@ var client = new Keen({
   readKey: "3f324dcb5636316d6865ab0ebbbbc725224c7f8f3e8899c7733439965d6d4a2c7f13bf7765458790bd50ec76b4361687f51cf626314585dc246bb51aeb455c0a1dd6ce77a993d9c953c5fc554d1d3530ca5d17bdc6d1333ef3d8146a990c79435bb2c7d936f259a22647a75407921056"
 });
 
+var geoProject = new Keen({
+  projectId: "53eab6e12481962467000000",
+  readKey: "d1b97982ce67ad4b411af30e53dd75be6cf610213c35f3bd3dd2ef62eaeac14632164890413e2cc2df2e489da88e87430af43628b0c9e0b2870d0a70580d5f5fe8d9ba2a6d56f9448a3b6f62a5e6cdd1be435c227253fbe3fab27beb0d14f91b710d9a6e657ecf47775281abc17ec455"
+});
+
+
+
 Keen.ready(function(){
 
   // ----------------------------------------
@@ -10,7 +17,6 @@ Keen.ready(function(){
   // ----------------------------------------
   var impressions_timeline = new Keen.Query("count", {
     eventCollection: "impressions",
-    groupBy: "ad.advertiser",
     interval: "hourly",
     timeframe: {
       start: "2014-05-04T00:00:00.000Z",
@@ -38,73 +44,93 @@ Keen.ready(function(){
     }
   });
 
+  // ----------------------------------------
+  // Heart Rate
+  // ----------------------------------------
 
-  // ----------------------------------------
-  // Impressions timeline (country)
-  // ----------------------------------------
-  var impressions_timeline_by_country = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "user.geo_info.country",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
+  var heart_rate = new Keen.Query("median", {
+    eventCollection: "user_action",
+    targetProperty: "bio_sensors.heart_rate",
+    filters: [
+      {
+        "property_name" : "user.id",
+        "operator" : "eq",
+        "property_value" : "01064B34-4626-58C7-B5D3-58F1B4F28575"
+      }]
   });
-  
-  client.draw(impressions_timeline_by_country, document.getElementById("chart-02"), {
-    chartType: "columnchart",
-    title: false,
-    height: 133,
-    width: "auto",
-    chartOptions: {
-      chartArea: {
-        left: "0%",
-        top: "0%",
-        height: "75%",
-        bottom: "0%",
-        width: "100%"
-      },
-      backgroundColor: 'transparent',
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
-    }
+
+  geoProject.run(heart_rate, function(res){
+    $(".heart").val(res.result).trigger('change');
+  });
+  $(function() {
+    $(".heart").knob({
+      'angleArc':250,
+      'angleOffset':-125,
+      'readOnly':true
+    });
   });
 
 
+
+
   // ----------------------------------------
-  // Impressions timeline (device)
+  // Temperature
   // ----------------------------------------
-  var impressions_timeline_by_device = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "user.device_info.device.family",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
+
+  var temperature = new Keen.Query("median", {
+    eventCollection: "user_action",
+    targetProperty: "enviro_sensors.temp",
+    filters: [
+      {
+        "property_name" : "user.id",
+        "operator" : "eq",
+        "property_value" : "01064B34-4626-58C7-B5D3-58F1B4F28575"
+      }]
   });
-  client.draw(impressions_timeline_by_device, document.getElementById("chart-03"), {
-    chartType: "columnchart",
-    title: false,
-    height: 133,
-    width: "auto",
-    chartOptions: {
-      chartArea: {
-        left: "0%",
-        top: "0%",
-        height: "75%",
-        bottom: "0%",
-        width: "100%"
-      },
-      backgroundColor: 'transparent',
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
-    }
+
+  geoProject.run(temperature, function(res){
+    $(".temp").val(res.result).trigger('change');
   });
+  $(function() {
+    $(".temp").knob({
+      'angleArc':250,
+      'angleOffset':-125,
+      'readOnly':true,
+      'draw': function (){
+        $(this.i).val(this.cv + '\u00B0')
+      }
+    });
+  });
+
+
+  // ----------------------------------------
+  // Battery
+  // ----------------------------------------
+
+  var battery = new Keen.Query("median", {
+    eventCollection: "user_action",
+    targetProperty: "battery_level",
+    filters: [
+      {
+        "property_name" : "user.id",
+        "operator" : "eq",
+        "property_value" : "01064B34-4626-58C7-B5D3-58F1B4F28575"
+      }]
+  });
+
+  geoProject.run(battery, function(res){
+    $(".battery").val((res.result)*100).trigger('change');
+  });
+  $(function() {
+    $(".battery").knob({
+      'angleArc':250,
+      'angleOffset':-125,
+      'readOnly':true,
+      'draw': function (){
+        $(this.i).val(this.cv + '%')
+      }
+    });
+  });
+
 
 });
