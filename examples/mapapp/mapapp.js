643951133485fdb18b7126f01b50e5f6bab44943
Dustin Larimer <dustinlarimer@gmail.com>
2014-08-15 15:58:03 -0700

    Update UI for prentation

--- examples/mapapp/mapapp.js
+++ examples/mapapp/mapapp.js
@@ -1,4 +1,6 @@
 !(function(undefined) {
+
+  // Maintain map dimensions
   window.onresize = adjust;
   function adjust(){
     document.getElementById("app-wrapper").style.height = window.innerHeight;
@@ -11,36 +13,61 @@
     readKey: "d1b97982ce67ad4b411af30e53dd75be6cf610213c35f3bd3dd2ef62eaeac14632164890413e2cc2df2e489da88e87430af43628b0c9e0b2870d0a70580d5f5fe8d9ba2a6d56f9448a3b6f62a5e6cdd1be435c227253fbe3fab27beb0d14f91b710d9a6e657ecf47775281abc17ec455"
   });
 
+  var DEFAULTS = {
+    coordinates: {
+      lat: 37.77350,
+      lng: -122.41104
+    },
+    zoom: 12
+  };
+
   Keen.ready(function() {
     var initialize,
         circle,
         marker,
         map,
-        markerStart = { lat: 47.586, lng: -122.319 },
+        markerStart = DEFAULTS.coordinates,
         latNode = document.getElementById('coordinates-lat'),
         lngNode = document.getElementById('coordinates-lng'),
         radiusValueNode = document.getElementById('radius-value'),
-        radiusSuffixNode = document.getElementById('radius-suffix'),
+        radiusUnitsNode = document.getElementById('radius-suffix'),
         timeframeStartNode = document.getElementById('timeframe-start'),
         timeframeEndNode = document.getElementById('timeframe-end');
 
     timeframeStartNode.value = "2014-08-01";
     timeframeEndNode.value = "2014-08-15";
 
+    // Listen for input changes
+    radiusValueNode.onchange = updateRadiusIndicator;
+    radiusUnitsNode.onchange = updateRadiusIndicator;
+    function updateRadiusIndicator(e){
+      var rangeInMeters = radiusValueNode.value / 1000,
+          radiusValue = radiusValueNode.value || 10,
+          radiusUnits = radiusUnitsNode.value || "km";
+      rangeInMeters = radiusValue * ((radiusUnits === "mi") ? 1609.34 : 1000);
+      circle.setRadius(rangeInMeters);
+    };
+
+    var activeMapData;
+
     var initialize = function() {
       L.mapbox.accessToken = "pk.eyJ1IjoiZG9ra28xMjMwIiwiYSI6IlM3TUN5RW8ifQ.wNT0Pp0zCtMR7phIRHg6Ug";
-
       map = L.mapbox.map("app-maparea", "dokko1230.j7ch6d77", {
-        attributionControl: true
+        attributionControl: true,
+        center: [markerStart.lat, markerStart.lng],
+        zoom: DEFAULTS.zoom
       });
 
-      marker = L.marker(new L.LatLng(47.586, -122.319), {
-          icon: L.mapbox.marker.icon({
-              "marker-color": "ff8888"
-          }),
-          draggable: true
-      });
+      // For query result markers
+      activeMapData = L.layerGroup().addTo(map);
 
+      marker = L.marker(new L.LatLng(markerStart.lat, markerStart.lng), {
+        icon: L.mapbox.marker.icon({
+          "marker-color": "ff8888"
+        }),
+        draggable: true,
+        zIndexOffset: 9999
+      });
       circle = L.circle([markerStart.lat, markerStart.lng], 1000);
       latNode.value = markerStart.lat;
       lngNode.value = markerStart.lng;
@@ -48,17 +75,21 @@
       marker.addTo(map);
       circle.addTo(map);
 
-      map.attributionControl
-        .addAttribution('<a href="https://keen.io/">Custom Analytics by Keen IO</a>');
+      updateRadiusIndicator();
+
+      map.attributionControl.addAttribution('<a href="https://keen.io/">Custom Analytics by Keen IO</a>');
 
       var keenMapData = L.layerGroup().addTo(map);
 
       marker.on('dragend', function(event) {
         // Move circle to marker
         var newCoords = event.target.getLatLng();
-        circle.setLatLng(newCoords);
-        latNode.value = newCoords.lat;
-        lngNode.value = newCoords.lng;
+        var newLat = newCoords.lat.toPrecision(8);
+        var newLng = newCoords.lng.toPrecision(8);
+
+        circle.setLatLng({ lat: newLat, lng: newLng });
+        latNode.value = newLat;
+        lngNode.value = newLng;
       });
 
       document.getElementById('refresh').onclick = refresh;
@@ -90,24 +121,12 @@
     var refresh = function() {
       var rangeInMeters, rangeInMiles,
           config,
-          radiusValue = radiusValueNode.value || 1000,
-          radiusSuffix = radiusSuffixNode.value || "km";
-
+          radiusValue = radiusValueNode.value || 10,
+          radiusUnits = radiusUnitsNode.value || "km";
 
       // Behold, the one-liner :)
-      rangeInMeters = radiusValue * (radiusSuffix === "mi") ? 1609.34 : 1000;
-      /*switch (radiusSuffix) { // Base is in meters
-      case 'Kilometers':
-        console.log(radiusValue, radiusValueNode.value);
-        rangeInMeters = radiusValue * 1000;
-        break;
-      case 'Miles':
-        rangeInMeters = radiusValue * 1609.34;
-        break;
-      }*/
-
-      // SET RANGE INDICATOR (Mapbox)
-      circle.setRadius(rangeInMeters);
+      rangeInMeters = radiusValue * ((radiusUnits === "mi") ? 1609.34 : 1000);
+      //circle.setRadius(rangeInMeters);
 
       // CONVERT TO MILES (API v3)
       rangeInMiles = rangeInMeters / 1609.34;
@@ -121,17 +140,14 @@
       };
 
       redraw(config);
-
       updateQuery(config);
     };
 
-    // Dustin, halp!
-    var redraw = function(config) {
+    function redraw(config){
       var options = config || {};
 
       var end = (options["end"]) ? new Date(Date.parse(options["end"])) : new Date();
       var start = (options["start"]) ? new Date(Date.parse(options["start"])) : new Date(end.getFullYear(), end.getMonth(), end.getDate()-14);
-
 
       var rad = (options["radius"]) ? parseFloat(options["radius"]) : false;
       var lat = (options["latitude"]) ? parseFloat(options["latitude"]) : false;
@@ -157,61 +173,85 @@
         filters: geoFilter
       };
 
-      // Make 3 queries
-      var pageviews_timeline = new Keen.Query("median", {
+      // Fetch events within geo target
+      var scoped_events = new Keen.Query("select_unique", {
         eventCollection: "user_action",
-        interval: "daily",
-        targetProperty: "bio_sensors.heart_rate",
-        //groupBy: "", // Not supported for Geo Filters
+        targetProperty: "keen.location.coordinates",
         timeframe: baseParams.timeframe,
         filters: baseParams.filters
       });
-      client.draw(pageviews_timeline, document.getElementById("chart-01"), {
+      client.run(scoped_events, function(res){
+        console.log("events", res);
+        activeMapData.clearLayers();
+
+        Keen.utils.each(res.result, function(coord, index){
+          var em = L.marker(new L.LatLng(coord[1], coord[0]), {
+            icon: L.mapbox.marker.icon({
+              "marker-color": Keen.Visualization.defaults.colors[0]
+            })
+          }).addTo(activeMapData);;
+        });
+      });
+
+      // Sample queries
+      // groupBy not supported for Geo Filters
+      var hearts = new Keen.Query("median", {
+        eventCollection: "user_action",
+        interval: "daily",
+        targetProperty: "bio_sensors.heart_rate",
+        timeframe: baseParams.timeframe,
+        filters: baseParams.filters
+      });
+      client.draw(hearts, document.getElementById("chart-01"), {
         chartType: "areachart",
-        title: null,
+        colors: [Keen.Visualization.defaults.colors[1]],
+        title: " ",
         height: 175,
         width: "auto",
-        colors: null,
         chartOptions: {
+          chartArea: { height: "80%", top: "5%", width: "80%" },
           hAxis: { format: 'MMM dd', maxTextLines: 1 },
           legend: { position: "none" },
           tooltip: { trigger: 'none' }
         }
       });
 
-      var impressions_timeline = new Keen.Query("count", {
+      var activations = new Keen.Query("count", {
         eventCollection: "activations",
         interval: "daily",
-        //groupBy: "", // Not supported for Geo Filters
         timeframe: baseParams.timeframe,
         filters: baseParams.filters
       });
-      client.draw(impressions_timeline, document.getElementById("chart-02"), {
-        chartType: "columnchart",
-        title: false,
+      client.draw(activations, document.getElementById("chart-02"), {
+        chartType: "areachart",
+        colors: [Keen.Visualization.defaults.colors[2]],
+        title: " ",
         height: 175,
-        width: 250,
+        width: "auto",
         chartOptions: {
           bar: { groupWidth: "85%" },
+          chartArea: { height: "80%", top: "5%", width: "80%" },
           hAxis: { format: 'MMM dd' },
           legend: { position: "none" },
           tooltip: { trigger: 'none' }
         }
       });
 
-      var impressions_timeline_by_country = new Keen.Query("count", {
+      var purchases = new Keen.Query("sum", {
         eventCollection: "purchases",
+        targetProperty: "order_price",
         interval: "daily",
         timeframe: baseParams.timeframe,
         filters: baseParams.filters
       });
-      client.draw(impressions_timeline_by_country, document.getElementById("chart-03"), {
+      client.draw(purchases, document.getElementById("chart-03"), {
         chartType: "columnchart",
         title: false,
         height: 175,
-        width: 250,
+        width: "auto",
         chartOptions: {
           bar: { groupWidth: "85%" },
+          chartArea: { height: "80%", top: "5%", width: "80%" },
           hAxis: { format: 'MMM dd' },
           legend: { position: "none" },
           tooltip: { trigger: 'none' }
